242b13f928b1d609099b09b7015f984caf7f3483
alaba98 <83726565+alaba98@users.noreply.github.com>
2024-10-07 20:29:13 +0100

    Initial commit with C# and F# code

---
+++ BasicInterpreter/Program.fs
@@ -0,0 +1,246 @@
+﻿module Interpreter2
+open System
+
+type terminal = 
+    | Add 
+    | Sub 
+    | Mul 
+    | Div 
+    | Pow 
+    | Rem 
+    | Lpar 
+    | Rpar 
+    | Num of float
+    | Vid of string
+    | Equ
+
+let str2lst s = [for c in s -> c]
+let isblank c = System.Char.IsWhiteSpace c
+let isdigit c = System.Char.IsDigit c
+let lexError = System.Exception("Lexer error")
+let intVal (c:char) = (int)((int)c - (int)'0')
+let ischar c = System.Char.IsLetter c // UPDATE
+let parseError = System.Exception("Parser error")
+let symError = System.Exception("No value associated to variable name")
+
+let rec scInt(iStr, iVal, frac, div) = 
+    match iStr with
+    | c :: tail when isdigit c -> 
+        if frac then
+            scInt(tail, iVal + (float (intVal c)) / div, frac, div * 10.0)
+        else
+            scInt(tail, 10.0 * iVal + (float (intVal c)), frac, div)
+
+    | '.' :: tail -> 
+        if frac then
+            raise lexError // Error: More than one decimal point in a number
+        else
+            scInt(tail, iVal, true, 10.0)
+    | _ -> (iStr, iVal)
+
+let rec scChar(iStr, vName:string) =
+    match iStr with
+    | c :: tail when ischar c -> scChar(tail,(vName + c.ToString()))
+    | _ -> (iStr, vName)
+let lexer input = 
+    let rec scan input =
+        match input with
+        | [] -> []
+        | '+'::tail -> Add :: scan tail
+        | '-'::tail -> 
+            match scan tail with
+            | Num num :: rest -> Num (-num) :: rest  // implicit Unary minus; 1st case, if the '-' is followed by a number, the number is negated and added to the 
+            | _ -> Sub :: scan tail  // Binary minus
+        | '*'::tail -> Mul :: scan tail
+        | '/'::tail -> Div :: scan tail  // Correctly tokenizing '/' as Div
+        | '^'::tail -> Pow :: scan tail
+        | '%'::tail -> Rem :: scan tail
+        | '('::tail -> Lpar:: scan tail
+        | ')'::tail -> Rpar:: scan tail
+        | '='::tail -> Equ:: scan tail  // UPDATE
+        | c :: tail when isblank c -> scan tail
+        | c :: tail when isdigit c || (c = '-' && List.isEmpty tail |> not && isdigit (List.head tail)) -> 
+            let (iStr, iVal) = scInt(tail, intVal c, false, 1.0) 
+            Num iVal :: scan iStr
+        | c :: tail when ischar c -> let (iStr, vName) = scChar(tail, c.ToString() ) // UPDATE
+                                     Vid vName :: scan iStr
+        | _ -> raise lexError
+    scan (str2lst input)
+
+
+
+let getInputString() : string = 
+    Console.Write("Enter an expression: ")
+    Console.ReadLine()
+
+// Grammar in (E)BNF 1st sprint:
+// <VA>       ::= <varID> "=" <E>
+// <E>        ::= <T> <Eopt>
+// <Eopt>     ::= "+" <T> <Eopt> | "-" <T> <Eopt> | <empty>
+// <T>        ::= <NR> <Topt>
+// <Topt>     ::= "*" <NR> <Topt> | "/" <NR> <Topt> | <empty>
+// <NR>       ::= ["Num" | "varVal" ] <value> | "(" <E> ")"
+// <varID>    ::= [a-z,A-Z]+     (* varVal is fetched from symbol table/list with key varID *)
+//----------------------------------------------------------------------
+//last sprint:
+//<varID>    ::= [a-z,A-Z]+
+//<VA> ::= <varID> "=" <E>
+//<digit> ::= 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+//<integer> ::= <digit> { <digit> }
+//<float> ::= <digit> { <digit> } "." { <digit> }
+//<value> ::= <float> | <integer>| varVal
+//<E> ::= <T> <Eopt>
+//<Eopt> ::= "+" <T> <Eopt> | "-" <T> <Eopt>| <empty>
+//<T> ::= <NR> <Topt>
+//<Topt> ::= "*" <NR> <Topt> | "/" <NR> <Topt> |"^" <NR> <Topt> | "%" <NR> <Topt> |  <empty>
+//<NR> ::= ("+"|"-")<NR> | <value>  | "(" <E> ")"
+//
+
+
+
+
+//
+//
+//
+
+let rec parser tList = 
+    let rec E tList = (T >> Eopt) tList
+    and Eopt tList =                        
+        match tList with
+        | Add :: tail -> (T >> Eopt) tail
+        | Sub :: tail -> (T >> Eopt) tail
+        | _ -> tList
+    and T tList = (NR >> Topt) tList
+    and Topt tList =
+        match tList with
+        | Mul :: tail -> (NR >> Topt) tail
+        | Div :: tail -> (NR >> Topt) tail
+        | Pow :: tail -> (NR >> Topt) tail
+        | Rem :: tail -> (NR >> Topt) tail
+        | _ -> tList
+    and NR tList =
+        match tList with 
+        | Num value :: tail -> tail
+        | Vid vName :: tail -> tail
+        | Lpar :: tail -> match E tail with 
+                          | Rpar :: tail -> tail
+                          | _ -> raise parseError
+        | _ -> raise parseError
+
+    let VA tList =  // UPDATE
+        match tList with
+        | Vid vName :: tail -> match tail with
+                               | Equ :: tail -> E tail // inner tail (from this line)
+                               | _ -> E tList  // Need tList to keep Vid vName
+        | _ -> E tList
+    VA tList  // CHANGED FROM E tList
+
+let rec searchVName vName (symList:List<string*float>) =
+    match symList with
+    | head :: tail -> if (fst head) = vName then (true, (snd head))
+                      else searchVName vName tail
+    | _ -> (false, 0)
+
+let rec parseNeval tList (symList:List<string*float>) = 
+    let rec E tList = (T >> Eopt) tList
+    and Eopt (tList, (vID, value)) = 
+        match tList with
+        | Add :: tail -> let (tLst, (vID, tval)) = T tail
+                         Eopt (tLst, (vID, value + tval))
+        | Sub :: tail -> let (tLst, (vID, tval)) = T tail
+                         Eopt (tLst, (vID, value - tval))
+        | _ -> (tList, ("", value))
+    and T tList = (NR >> Topt) tList
+    and Topt (tList, (vID, value)) =
+        match tList with
+        | Mul :: tail -> let (tLst, (vID, tval)) = NR tail
+                         Topt (tLst, (vID, value * tval))
+        | Div :: tail -> let (tLst, (vID, tval)) = NR tail
+                         if tval = 0.0 then
+                             raise (System.DivideByZeroException("Division by zero is undefined"))
+                         else
+                             Topt (tLst, (vID, value / tval)) 
+        | Pow :: tail -> let (tLst, (vID, tval)) = NR tail
+                         Topt (tLst, (vID, value ** tval))
+        | Rem :: tail -> let (tLst, (vID, tval)) = NR tail
+                         Topt (tLst, (vID, value % tval))
+        | _ -> (tList, ("", value))
+    and NR tList =
+        match tList with 
+        | Num value :: tail -> (tail, ("", value))
+        | Vid vName :: tail -> let res = searchVName vName symList
+                               if (fst res) then (tail, ("",(snd res)))
+                               else raise symError
+        | Lpar :: tail -> let (tLst, tval) = E tail
+                          match tLst with 
+                          | Rpar :: tail -> (tail, tval)
+                          | _ -> raise parseError
+        | _ -> raise parseError
+    let VA tList = 
+        match tList with 
+        | Vid vName :: tail -> match tail with 
+                               | Equ :: tail -> let (tLst, (vID, tval)) = E tail
+                                                (tLst, (vName, tval))
+                               | _ -> E tList
+        | _ -> E tList
+    VA tList
+    // UPDATE from E tList 
+
+
+
+
+let rec printTList (lst:list<terminal>) : list<string> = 
+    match lst with
+    head::tail -> Console.Write("{0} ",head.ToString())
+                  printTList tail
+                  
+    | [] -> Console.Write("EOL\n")
+            []
+
+
+let rec check4vid sList vName value =  // added to update symbol table list if already existing vName is overwritten
+    match sList with
+    | head :: tail -> if (fst head) = vName then [(vName,value)]@(check4vid tail vName value) // replace original value
+                      else [head]@(check4vid tail vName value) // copy original value
+    | _ -> []
+
+let rec printSymTList (sList:List<string*float>)  =
+    match sList with
+    | head :: [] -> Console.Write("{0}", head)
+                    printSymTList []
+    | head :: tail -> Console.Write("{0};", head)
+                      printSymTList tail
+    | [] -> Console.WriteLine("]")
+
+let rec inpLoop (symTList:List<string*float>) = 
+    Console.Write("Symbol Table = [")
+    let outSym = printSymTList symTList
+    let input = getInputString()
+    if input <> "" then
+        let oList = lexer input
+        let sList = printTList oList
+        let pList = parser oList  // pList is the remaining token list and should be empty
+        if not pList.IsEmpty then raise parseError // NOTE this update to avoid expressions like 3(2+3) that would return a value of 3 and have a nonempty token list ([Lpar Num 2 Add Num 3 Rpar], 3)
+        let Out = parseNeval oList symTList
+        let tempID = fst (snd Out)
+        let tempVal = (snd (snd Out))
+        Console.WriteLine("Variable name = {0}", tempID)    // UPDATE
+        Console.WriteLine("Result = {0}", tempVal)          // UPDATED
+        // Check whether variable name was already in symTList and if so replace with new value
+        if tempID.Length > 0 then // update symbol table
+            if symTList.IsEmpty then 
+                inpLoop (symTList@[tempID, tempVal])  // append new value if symbol table is empty
+            else 
+                let res = check4vid symTList tempID tempVal // if tempID is already in symbol table replace its value
+                let check = res.Equals(symTList)      // Check whether res is equal to the original (means no replacing was done)
+                if check then inpLoop (symTList@[tempID, tempVal])  // if true pass old list with appended new tuple                 
+                else inpLoop res   // if false pass updated res list with updated tuple
+        else inpLoop symTList 
+    else symTList
+
+[<EntryPoint>]
+let main argv  =
+    Console.WriteLine("Simple Interpreter")
+    let res = inpLoop [] 
+    Console.WriteLine("Symbol table is {0}", res )
+    0
---
+++ InterpreterGUI/MainWindow.xaml.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.FSharp.Collections;
+using OxyPlot;
+using OxyPlot.Series;
+using System.Text.RegularExpressions;
+
+namespace InterpreterGUI
+{
+
+    public partial class MainWindow : Window
+    {
+        private Dictionary<string, string> symbolTable = new Dictionary<string, string>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void EvaluateButton_Click(object sender, RoutedEventArgs e)
+        {
+            string expression = ExpressionTextBox.Text;
+            try
+            {
+                var lexerResult = Interpreter2.lexer(expression);
+                var parserResult = Interpreter2.parser(lexerResult);
+
+                // Before passing the symbolTable to parseNeval, convert numeric string values to double
+                var fsharpList = ListModule.OfSeq(symbolTable
+                    .Where(kv => !string.IsNullOrEmpty(kv.Value) && double.TryParse(kv.Value, out _))
+                    .Select(kv => Tuple.Create(kv.Key, double.Parse(kv.Value))));
+
+                var evaluationResult = Interpreter2.parseNeval(lexerResult, fsharpList);
+                float result = (float)evaluationResult.Item2.Item2;
+                string variableName = evaluationResult.Item2.Item1;
+
+                // If the variable name is not empty, update the symbol table with the result
+                if (!string.IsNullOrEmpty(variableName))
+                    symbolTable[variableName] = result.ToString();
+
+                ResultsTextBox.Text = $"{expression} = {result}";
+                UpdateSymbolTableLabel();
+                ErrorsTextBox.Text = ""; // Clear any previous errors
+            }
+            catch (Exception ex)
+            {
+                ResultsTextBox.Text = ""; // Clear any previous results
+                ErrorsTextBox.Text = $"Error: {ex.Message}";
+            }
+        }
+
+        private void AssignFunctionButton_Click(object sender, RoutedEventArgs e)
+        {
+            string function = FunctionTextBox.Text;
+            try
+            {
+                // Split the function expression to get the variable name and the function
+                string[] parts = function.Split('=');
+                if (parts.Length == 2 && parts[0].Trim() == "y")
+                {
+                    // Extract the function expression
+                    string functionExpression = parts[1].Trim();
+
+                    // Store the function expression as a string in the symbol table with the variable name "y"
+                    symbolTable["y"] = functionExpression;
+
+                    UpdateSymbolTableLabel(); // Update the display of the symbol table
+                                              // Optionally, you can provide feedback to the user to indicate that the function has been assigned.
+                    MessageBox.Show("Function assigned successfully!", "Assignment Status", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    // If the input is not in the correct format or if the variable name is not "y", show an error message to the user
+                    MessageBox.Show("Invalid input format. Please enter the assignment in the format 'y = expression'.", "Input Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that may occur during the assignment process
+                MessageBox.Show($"Error assigning function: {ex.Message}", "Assignment Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+
+        private void Help_Click(object sender, RoutedEventArgs e)
+        {
+            // Explanation of valid tokens and syntax
+            string helpMessage = "Valid Tokens:\n" +
+                                 "+ - * / ^ % ( )\n" +
+                                 "Numeric values\n" +
+                                 "Variable names (e.g., x, y)\n\n" +
+                                 "Syntax:\n" +
+                                 "<varID>    ::= [a-z,A-Z]+ \n" +
+                                 "<VA> ::= <varID>  =  <E> \n" +
+                                 "<digit> ::= 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 \n " +
+                                 "<integer> ::= <digit> { <digit> } \n" +
+                                 "<float> ::= <digit> { <digit> } . { <digit> } \n " +
+                                 "<value> ::= <float> | <integer>| varVal \n" +
+                                 "<E> ::= <T> <Eopt> \n" +
+                                 "<Eopt> ::=  +  <T> <Eopt> |  -  <T> <Eopt>| <empty> \n" +
+                                 "<T> ::= <NR> <Topt> \n " +
+                                 "<Topt> ::=  *  <NR> <Topt> |  /  <NR> <Topt> | ^  <NR> <Topt> |  %  <NR> <Topt> |  <empty>" +
+                                 "<NR> ::= ( + | - )<NR> | <value>  | ( <E> ) \n ";
+
+            // Show the help message in a message box
+            MessageBox.Show(helpMessage, "Syntax and Tokens", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void UpdateSymbolTableLabel()
+        {
+            SymbolTableTextBox.Text = "";
+            foreach (var entry in symbolTable)
+            {
+                SymbolTableTextBox.Text += $"{entry.Key} = {entry.Value}\n";
+            }
+        }
+
+        private void DrawFunctionButton_Click(object sender, RoutedEventArgs e)
+        {
+            string function = FunctionTextBox.Text.Trim();
+
+            // Check if the function string is empty
+            if (string.IsNullOrEmpty(function))
+            {
+                MessageBox.Show("Please enter a function before drawing.", "No Function Entered", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            try
+            {
+                // Clear previous series from the plot
+                FunctionPlot.Model.Series.Clear();
+                double startX, endX, stepX;
+
+                // Parse the x range input from the text boxes
+                if (!double.TryParse(StartXTextBox.Text, out startX) ||
+                    !double.TryParse(EndXTextBox.Text, out endX) ||
+                    !double.TryParse(StepXTextBox.Text, out stepX))
+                {
+                    MessageBox.Show("Invalid input for x range.", "Input Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                // Create a new LineSeries for the function plot
+                var series = new LineSeries();
+
+
+                // Check if the function is linear
+                if (IsLinear(function))
+                {
+                    // Generate points for the linear function and add them to the series
+                    for (double x = startX; x <= endX; x += stepX)
+                    {
+                        double y = EvaluateLinear(function, x);
+                        series.Points.Add(new DataPoint(x, y));
+                    }
+                }
+                else
+                {
+                    // Generate points for the polynomial function and add them to the series
+                    for (double x = startX; x <= endX; x += stepX)
+                    {
+                        double y = EvaluatePolynomial(function, x);
+                        series.Points.Add(new DataPoint(x, y));
+                    }
+                }
+
+
+                // Add the series to the plot
+                FunctionPlot.Model.Series.Add(series);
+
+                // Refresh the plot
+                FunctionPlot.InvalidatePlot(true);
+            }
+            catch (Exception ex)
+            {
+                // Handle any errors that may occur during function evaluation or plotting
+                MessageBox.Show($"Error drawing function: {ex.Message}", "Drawing Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+
+        private void FunctionPlot_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Ensure that the PlotModel is initialized
+            FunctionPlot.Model = new PlotModel();
+        }
+
+        private bool IsLinear(string function)
+        {
+            // Check if the function contains terms like "x" or "mx + c"
+            return Regex.IsMatch(function, @"\bx\b|^\s*-?\d*x\s*(\+\s*\d+)?\s*$");
+        }
+
+        private double EvaluatePolynomial(string function, double x)
+        {
+            // Parse the function string to extract coefficients and exponents
+            MatchCollection matches = Regex.Matches(function, @"(?<coef>-?\d*)\*?x\^?(?<exp>\d+)?");
+
+            double result = 0;
+            foreach (Match match in matches)
+            {
+                double coef = string.IsNullOrEmpty(match.Groups["coef"].Value) ? 1 : double.Parse(match.Groups["coef"].Value);
+                int exp = string.IsNullOrEmpty(match.Groups["exp"].Value) ? 1 : int.Parse(match.Groups["exp"].Value);
+
+                result += coef * Math.Pow(x, exp);
+            }
+
+            return result;
+       
+            //   - (?<coef>-?\d*): Named capture group 'coef' matches an optional minus sign ('-') followed by zero or more digits ('\d*'). 
+            //                      It captures the coefficient of the polynomial term.
+            //   - \*?: Matches the multiplication operator '*' literally, allowing for an optional multiplication operator between the coefficient and the variable 'x'.
+            //   - x: Matches the variable 'x' literally.
+            //   - \^?: Matches the exponentiation operator '^' literally, allowing for an optional exponentiation operator after the variable 'x'.
+            //   - (?<exp>\d+)?:
+            //     - (?<exp>: Named capture group 'exp' matches one or more digits ('\d+'). It captures the exponent of the polynomial term.
+            //     - ?: Makes the entire group optional to handle cases where there is no explicit exponent for a term.
+            
+
+        }
+
+        private double EvaluateLinear(string function, double x)
+        {
+            // Parse the function string to extract slope and intercept
+            Match match = Regex.Match(function, @"(?<m>-?\d*)x\s*(\+\s*(?<c>-?\d+))?");
+
+            double m = string.IsNullOrEmpty(match.Groups["m"].Value) ? 0 : double.Parse(match.Groups["m"].Value);
+            double c = string.IsNullOrEmpty(match.Groups["c"].Value) ? 0 : double.Parse(match.Groups["c"].Value);
+
+            return m * x + c;
+        
+            //   - (?<m>-?\d*): Named capture group 'm' matches an optional minus sign ('-') followed by zero or more digits ('\d*'). 
+            //                  It captures the slope coefficient.
+            //   - x\s*: Matches the character 'x' followed by zero or more whitespace characters ('\s*').
+            //   - (\+\s*(?<c>-?\d+))?:
+            //     - (\+\s*): Non-capturing group matches a plus sign ('+') followed by zero or more whitespace characters ('\s*').
+            //     - (?<c>-?\d+): Named capture group 'c' matches an optional minus sign ('-') followed by one or more digits ('\d+').
+            //                    It captures the intercept coefficient.
+            //     - ?: Makes the entire group optional to handle cases where there is no intercept term.
+           
+
+        }
+
+    }
+}
